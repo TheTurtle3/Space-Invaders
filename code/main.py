4514214b6ea3d4aebf7d661bfe354b49fa769bac
Ahmad Zafar <amz9616@gmail.com>
2023-01-03 10:51:00 -0500

    init

---
+++ code/main.py
@@ -0,0 +1,29 @@
+import pygame, sys
+
+class Game:
+    def __init__(self) -> None:
+        pass
+
+    def run(Self):
+        pass
+
+if __name__ == '__main__':
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    pygame.display.set_caption('Space Invader')
+    clock = pygame.time.Clock()
+    game = Game()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((30,30,30))
+        game.run()
+
+        pygame.display.flip()
+        clock.tick(60)
